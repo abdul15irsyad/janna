27ce278e5fe99e641ba7513fa067756b16b17d91
Irsyad Abdul <abdulirsyad15@gmail.com>
2023-03-17 13:52:23 +0700

    try caching with cache-manager

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    @Inject() private appService: AppService;
+    @Inject(CACHE_MANAGER) private cacheManager: Cache;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    async root() {
+        let rootMessage = await this.cacheManager.get('rootMessage');
+        if (!rootMessage) {
+            rootMessage = this.appService.rootMessage();
+            await this.cacheManager.set('rootMessage', rootMessage, 15 * 1000);
+        };
+
+        return {
+            message: rootMessage,
+        };
+    }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
