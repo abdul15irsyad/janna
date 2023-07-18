2cd75394cbda5f050a2521162e404b983d94ba3c
Irsyad Abdul <abdulirsyad15@gmail.com>
2023-07-18 20:25:19 +0700

    add createdAt at order by user

--- src/user/dto/find-all-user.dto.ts
+++ src/user/dto/find-all-user.dto.ts
@@ -3,10 +3,10 @@ import { FindAllDto } from '../../shared/dto/find-all.dto';
 import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class FindAllUserDto extends FindAllDto {
-  @IsIn(['name', 'email'], {
+  @IsIn(['name', 'email', 'createdAt'], {
     message: i18nValidationMessage('validation.IS_IN'),
   })
   @IsString({ message: i18nValidationMessage('validation.IS_STRING') })
   @IsOptional()
-  orderBy?: 'name' | 'email';
+  orderBy?: 'name' | 'email' | 'createdAt';
 }
