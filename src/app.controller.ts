c4fdbc048dd0b4d5f1f17d435e2f4b65ffee2d34
Irsyad Abdul <abdulirsyad15@gmail.com>
2023-07-18 21:28:13 +0700

    delete test in root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,7 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import { handleError } from './shared/utils/error.util';
 import { MailService } from './mail/mail.service';
-import { I18n, I18nContext, i18nValidationMessage } from 'nestjs-i18n';
-import { I18nTranslations } from './i18n/i18n.generated';
-import { IsNotEmpty } from 'class-validator';
-
-class RootDto {
-  @IsNotEmpty({ message: i18nValidationMessage('validation.IS_NOT_EMPTY') })
-  test: string;
-}
 
 @Controller()
 export class AppController {
@@ -17,18 +9,11 @@ export class AppController {
   @Inject(MailService) private mailService: MailService;
 
   @Get()
-  async root(
-    @Query() rootDto: RootDto,
-    @I18n() i18n: I18nContext<I18nTranslations>,
-  ) {
+  async root() {
     try {
       const rootMessage = this.appService.rootMessage();
-      const helloWorld = this.appService.helloWorld();
-
       return {
         message: rootMessage,
-        helloWorld,
-        helloWorld2: i18n.t('common.HELLO_WORLD'),
       };
     } catch (error) {
       handleError({ error });
